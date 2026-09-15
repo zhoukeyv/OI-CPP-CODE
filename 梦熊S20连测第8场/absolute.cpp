6ca9b131d461ce4c636b3d8e9c3c071d34c4bc6a
zhoukeyv <coder123@outlook.com>
2026-09-15 10:10:35 +0800

    Auto-commit: ".cph-ng/C2027\347\272\247\350\256\255\347\273\2032/A - \346\213\257\346\225\221\345\260\217\347\237\256\344\272\272.cpp.bin", "\346\242\246\347\206\212S20\350\277\236\346\265\213\347\254\2548\345\234\272/absolute.cpp" at 2026/9/15 10:10:35

---
+++ 梦熊S20连测第8场/absolute.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define double long double
+#define int long long
+using namespace std;
+const double eps=1e-6;
+const int inf=1e18;
+void solve()
+{
+    
+    return;
+}
+signed main()
+{
+#ifndef LOCAL_cph
+    // freopen(".in","r",stdin);
+    // freopen(".out","w",stdout);
+#endif
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int TestCase=1;
+    // cin>>TestCase;
+    for (int Caseid=1;Caseid<=TestCase;Caseid++)
+    {
+        solve();
+    }
+    return 0;
+}

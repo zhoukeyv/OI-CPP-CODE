a769431980c156a8b2be6a8221dbfae7fea898f0
zhoukeyv <coder123@outlook.com>
2026-09-15 10:46:25 +0800

    Update card.cpp to enhance functionality and improve code readability

--- 梦熊S20连测第8场/card.cpp
+++ 梦熊S20连测第8场/card.cpp
@@ -12,8 +12,8 @@ void solve()
 signed main()
 {
 #ifndef LOCAL_cph
-	// freopen(".in","r",stdin);
-	// freopen(".out","w",stdout);
+	freopen("card.in","r",stdin);
+	freopen("card.out","w",stdout);
 #endif
 	ios::sync_with_stdio(0);
 	cin.tie(0);

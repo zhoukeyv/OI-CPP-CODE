60a92dbea94495bd7c18d868d5ac0501dc6b3140
zhoukeyv <coder123@outlook.com>
2026-09-15 10:46:57 +0800

    Update card.cpp to improve functionality and enhance performance

--- 梦熊S20连测第8场/card.cpp
+++ 梦熊S20连测第8场/card.cpp
@@ -3,9 +3,12 @@
 #define int long long
 using namespace std;
 const double eps=1e-6;
-const int inf=1e18;
+const int inf=1e18,N=3e5+10;
+int a[N],b[N];
+int n;
 void solve()
 {
+	cin>>n;
 	
 	return;
 }

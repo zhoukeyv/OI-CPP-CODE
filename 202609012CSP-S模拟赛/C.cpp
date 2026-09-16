766150f743f8337b8b47bc98c8c814eca137a0cb
zhoukeyv <coder123@outlook.com>
2026-09-16 15:50:09 +0800

    Auto-commit: ".cph-ng/C2027\347\272\247\350\256\255\347\273\2032/B - \350\213\261\351\233\204\350\201\224\347\233\237.cpp.bin", "202609012CSP-S\346\250\241\346\213\237\350\265\233/C.cpp" at 2026/9/16 15:50:09

---
+++ 202609012CSP-S模拟赛/C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define double long double
+#define int long long
+using namespace std;
+const double eps=1e-6;
+const int inf=1e18,N=2e5+10;
+vector<int> graph[N];
+int n;
+void solve()
+{
+	cin>>n;
+	return;
+}
+signed main()
+{
+#ifndef LOCAL_cph
+	// freopen(".in","r",stdin);
+	// freopen(".out","w",stdout);
+#endif
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int TestCase=1;
+	// cin>>TestCase;
+	for (int Caseid=1;Caseid<=TestCase;Caseid++)
+	{
+		solve();
+	}
+	return 0;
+}

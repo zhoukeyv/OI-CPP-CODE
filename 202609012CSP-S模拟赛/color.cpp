8c9350f7470fd00a46a7a9bd4af6a833efc2be80
zhoukeyv <coder123@outlook.com>
2026-09-16 16:39:29 +0800

    Update color handling logic and improve performance of color processing functions

--- 202609012CSP-S模拟赛/color.cpp
+++ 202609012CSP-S模拟赛/color.cpp
@@ -122,15 +122,15 @@ int merge(int idx1,int idx2,int l,int r)
 	push_down(idx1,l,r);
 	push_down(idx2,l,r);
 	int mid=l+(r-l)/2;
-	if (tr[idx].l==-1)
+	if (tr[idx1].l==-1)
 	{
 		int lc=new_node();
-		tr[idx].l=lc;
+		tr[idx1].l=lc;
 	}
-	if (tr[idx].r==-1)
+	if (tr[idx1].r==-1)
 	{
 		int rc=new_node();
-		tr[idx].r=rc;
+		tr[idx1].r=rc;
 	}
 	tr[idx1].l=merge(tr[idx1].l,tr[idx2].l,l,mid);
 	tr[idx1].r=merge(tr[idx1].r,tr[idx2].r,mid+1,r);

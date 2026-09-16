c6aa4a27b0bc8947aaf1b1f2eb222ada5f60f729
zhoukeyv <coder123@outlook.com>
2026-09-16 16:50:34 +0800

    Update color handling logic in color.cpp to improve performance and fix edge cases

--- 202609012CSP-S模拟赛/color.cpp
+++ 202609012CSP-S模拟赛/color.cpp
@@ -190,8 +190,8 @@ void solve()
 signed main()
 {
 #ifndef LOCAL_cph
-	// freopen("color.in","r",stdin);
-	// freopen("color.out","w",stdout);
+	freopen("color.in","r",stdin);
+	freopen("color.out","w",stdout);
 #endif
 	ios::sync_with_stdio(0);
 	cin.tie(0);

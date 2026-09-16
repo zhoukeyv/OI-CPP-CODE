6802bc00df031e00e8984f216d500ce50868e657
zhoukeyv <coder123@outlook.com>
2026-09-16 16:24:37 +0800

    Update color handling in color.cpp to improve performance and fix bugs

--- 202609012CSP-S模拟赛/color.cpp
+++ 202609012CSP-S模拟赛/color.cpp
@@ -78,6 +78,16 @@ void update(int idx,int l,int r,int x,int v)
 		}
 		update(tr[idx].l,l,mid,x,v);
 	}
+	else
+	{
+		if (tr[idx].r==-1)
+		{
+			int rc=new_node();
+			tr[idx].r=rc;
+		}
+		update(tr[idx].r,mid+1,r,x,v);
+	}
+	return;
 }
 vector<int> graph[N];
 int dp[N];

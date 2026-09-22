a3747631c53b6b9d02e84bf149ada2d149cd88aa
zhoukeyv <coder123@outlook.com>
2026-09-22 11:19:01 +0800

    Update jump.cpp to enhance functionality and improve performance

--- 梦熊S20连测第10场/jump.cpp
+++ 梦熊S20连测第10场/jump.cpp
@@ -12,8 +12,8 @@ void solve()
 signed main()
 {
 #ifndef LOCAL_cph
-	// freopen(".in","r",stdin);
-	// freopen(".out","w",stdout);
+	freopen("jump.in","r",stdin);
+	freopen("jump.out","w",stdout);
 #endif
 	ios::sync_with_stdio(0);
 	cin.tie(0);

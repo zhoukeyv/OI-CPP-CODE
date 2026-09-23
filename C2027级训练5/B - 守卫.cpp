bc00a839ff735ab717e3cb5d903ce96eac2d7eed
zhoukeyv <coder123@outlook.com>
2026-09-23 16:07:52 +0800

    Update code logic and improve performance in main algorithm

--- C2027级训练5/B - 守卫.cpp
+++ C2027级训练5/B - 守卫.cpp
@@ -7,6 +7,18 @@ const int inf=1e18,N=5010;
 int dp[N][N];
 int a[N];
 int n;
+int DP(int l,int r)
+{
+	if (dp[l][r]!=-1)
+	{
+		return dp[l][r];
+	}
+	if (l==r)
+	{
+		return dp[l][r]=1;
+	}
+	dp[l][r]=0;
+}
 void solve()
 {
 	cin>>n;

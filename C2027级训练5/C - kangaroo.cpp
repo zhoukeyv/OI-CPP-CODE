d03cb90808a70c608ae68d5a4cbecabd32becf05
zhoukeyv <coder123@outlook.com>
2026-09-24 15:52:44 +0800

    Update kangaroo.cpp to improve functionality and enhance performance

--- C2027级训练5/C - kangaroo.cpp
+++ C2027级训练5/C - kangaroo.cpp
@@ -15,6 +15,7 @@ void solve()
 		for (int j=1;j<=i;j++)
 		{
 			dp[i][j]=((j-(i>s)-(i>t))*dp[i-1][j-1]%mod+dp[i-1][j+1]*j%mod)%mod;
+			cerr<<i<<' '<<j<<' '<<dp[i][j]<<'\n';
 		}
 	}
 	cout<<dp[n][1]<<'\n';
